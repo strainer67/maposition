0fdb804950797fa350f60b47442aa14079a8572c
romain <romain.pestourie@gmx.fr>
2020-01-06 23:42:03 +0100

    propagate modif

--- test.py
+++ test.py
@@ -10,9 +10,9 @@ if __name__ == "__main__":
     url_base = 'http://127.0.0.1:5000/send_position?'
     user_id = 1
     while True:
-        my_lat = round(random.uniform(48.55392, 48.61392), 5)
-        my_lon = round(random.uniform(7.71553, 7.775530), 5)
-        my_time = datetime.now().strftime('%d-%m-%Y %H:%M:%S')
+        my_lat = round(random.uniform(48.55392, 48.61392), 8)
+        my_lon = round(random.uniform(7.71553, 7.775530), 8)
+        my_time = datetime.now().strftime('%Y-%m-%dT%H:%M:%S.%fZ')
         content = f'lat={my_lat}&lon={my_lon}&time={my_time}&user_id={user_id}'
         data = f'{url_base}{content}'
         r = requests.get(data)
